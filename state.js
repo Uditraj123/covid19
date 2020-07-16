22e6c7758c7320a49d1998b69f98b197389f73dd
Uditraj123 <65947142+Uditraj123@users.noreply.github.com>
2020-07-16 14:42:42 +0530

    Add files via upload

--- state.js
+++ state.js
@@ -1,6 +1,11 @@
 //add application interface for indian statewise results
 var myArr;
 var state_name=document.getElementById("state_name");
+//chart k liye kaam hai
+var state=[];
+        var confirmed=[];
+        var recovered=[];
+        var deaths=[]; 
 function requestData(){
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.open("GET","https://api.rootnet.in/covid19-in/stats/latest");
@@ -10,6 +15,17 @@ function requestData(){
         console.log(myArr);
         requestRegional(myArr.data.regional)
         console.log(myArr.data.regional);
+
+        //chart k liye kaam hai
+        var confirmedCasesIndian,deaths,discharged,active;
+        discharged=myArr.data.summary.discharged;
+        deaths=myArr.data.summary.deaths;
+        confirmedCasesIndian=myArr.data.summary.confirmedCasesIndian;
+        active=myArr.data.summary.confirmedCasesIndian-myArr.data.summary.discharged;
+        console.log(discharged);
+        console.log(active);
+        //..........
+
     }
 }
 function requestRegional(stateData){
@@ -24,8 +40,77 @@ function requestRegional(stateData){
             displayData(stateData[i]);
         }
         state_name.appendChild(option); 
+        //chart k liye kaam hai
+        state.push(stateData[i].loc);
+        deaths.push(stateData[i].deaths);
+        recovered.push(stateData[i].discharged);
+        confirmed.push(stateData[i].totalConfirmed);
+//chart formation and initialization
+        var myChart=document.getElementById("myChart").getContext('2d');
+        var Char=document.getElementById("Chart").getContext('2d');
+        var mychat=document.getElementById("myChat").getContext('2d');
+
+        var chart=new Chart(Char,{
+            type:'line',
+            backgroundColor:"#f5deb3",
+            data:{
+                labels:state,
+                datasets:[
+                    {
+                        label:"recovered cases",
+                        data:recovered,
+                        backgroundColor:"#2ecc71",
+                        minBarLength:5,
+                    },
+                ],
+            },
+            options:{}
+        });
+
+
+    
+    var chart=new Chart(myChat,{
+        type:'line',
+        backgroundColor:"#f5deb3",
+        data:{
+            labels:state,
+            datasets:[
+                {
+                    label:"deaths cases",
+                    data:deaths,
+                    backgroundColor:"#e74c3c",
+                    minBarLength:5,
+                },
+            ],
+        },
+        options:{}
+    });
+
+        var chart=new Chart(myChart,{
+            type:'line',
+            backgroundColor:"#f5deb3",
+            data:{
+                fontColor:'white',
+                labels:state,
+                datasets:[
+                    {
+                        label:"confirmed cases",
+                        data:confirmed,
+                        backgroundColor:"#f1c40f",
+                        minBarLength:5,
+                        
+                    
+                    },
+                ],
+            },
+            options:{}
+            
+        });
     }
+    console.log(state);
 }
+//..............
+
 function displayData(obj){
     document.getElementById("confirmed").innerHTML=obj.totalConfirmed;
     document.getElementById("recovered").innerHTML=obj.discharged;
